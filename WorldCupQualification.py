04f7ae5a1898d053d13189ea544a5035a48f8f82
rajat3illinois <rajat3@illinois.edu>
2021-04-28 01:47:10 -0500

    African Tentative teams are output. Need to perform critical analysis for the rest of the considerations. Thinking of implementing a better design for the process.

--- WorldCupQualification.py
+++ WorldCupQualification.py
@@ -42,13 +42,14 @@ class Team:
         team_details = pd.merge(team, team_rankings, left_on='Nation', right_index=True, how='left')
         return Players.team_player_details(team_details)
 
-    def qualified_teams(self, player_team_merged_frame: pd.DataFrame) -> list:
+    @staticmethod
+    def qualified_teams(player_team_merged_frame: pd.DataFrame, confideration_count: int):
         """
-        
+
         :param player_team_merged_frame:
-        :return: 
+        :param confideration_count:
+        :return:
         """
-        confideration = {'CAF': 5, 'AFC': [4, 1], 'UEFA': 13, 'CONMEBOL': [4, 1], 'CONCACAF': [3, 1], 'OFC': 1}
 
         max_rank = player_team_merged_frame.max()['Rank']
         min_rank = player_team_merged_frame.min()['Rank']
@@ -59,6 +60,17 @@ class Team:
         teams_qualified = player_team_merged_frame.loc[(player_team_merged_frame['Normalize_Rank'].gt(0.4)) &
                                                   ((player_team_merged_frame['Squad_Size'] * 1.5) < player_team_merged_frame[
                                                       'Above_Median_Count'])]
+        player_team_merged_frame['Total Value'] = player_team_merged_frame['Total Value'].replace({'\$': ''}, regex=True)
+        player_team_merged_frame['Total Value'] = player_team_merged_frame['Total Value'].replace({'m': ''}, regex=True)
+
+        player_team_merged_frame['Total Value'] = player_team_merged_frame['Total Value'].astype(float)
+        different_teams_qualified = player_team_merged_frame.loc[(player_team_merged_frame['Normalize_Rank'].gt(0.4)) &
+                                                                 (player_team_merged_frame['Total Value'].gt(150))]
+        different_values = different_teams_qualified[~different_teams_qualified.isin(teams_qualified)].dropna()
+        frames = [teams_qualified, different_values]
+        final_result = pd.concat(frames)
+        pick_random_teams = final_result.sample(n=confideration_count).reset_index()
+        qualified_teams.append(pick_random_teams['Nation'].tolist())
 
 
 class Players:
@@ -84,13 +96,20 @@ class MatchResult:
 
 if __name__ == '__main__':
 
+    qualified_teams = []
+    confideration = {'CAF': 5, 'AFC': 5, 'UEFA': 13, 'CONMEBOL': 5, 'CONCACAF': 4, 'OFC': 1}
     team_rankings = pd.read_csv("Latest_Rankings.csv", index_col='Nation')
     africa_teams = pd.read_csv("AFC.csv")
-    africa_teams_details = Team.fetch_team_details(africa_teams)
     players_df = pd.read_csv("players_20.csv", index_col="sofifa_id")
+    Team.qualified_teams(Team.fetch_team_details(africa_teams), confideration['CAF'])
+    print(qualified_teams)
 
-    Asia_teams = pd.read_csv("Asia.csv")
+    asia_teams = pd.read_csv("Asia.csv")
+    Team.qualified_teams(Team.fetch_team_details(asia_teams), confideration['AFC'])
+    print(qualified_teams)
     North_America_teams = pd.read_csv("North_America.csv")
+    Team.qualified_teams(Team.fetch_team_details(North_America_teams), confideration['CONCACAF'])
+    print(qualified_teams)
     South_America_teams = pd.read_csv("South_America.csv")
     Oceania_Teams = pd.read_csv("Oceania.csv")
     UEFA_Teams = pd.read_csv("UEFA.csv")
